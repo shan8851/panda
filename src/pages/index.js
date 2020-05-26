96082157219de500b5b5cd37017353702d0c1ec1
shan5742 <asamshan456@gmail.com>
2020-05-26 07:39:30 +0100

    home page redesign done

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,31 @@ import React from "react"
 import Layout from "../components/shared/layout"
 import SEO from "../components/shared/seo"
 import styled from "styled-components"
-import RoundLogo from "../images/pandalogo2.png"
+import Contact from "../components/shared/contact"
+import Paul from "../components/shared/paul"
+import ServicesHome from "../components/shared/servicesHome"
+import { BoldText } from "../components/shared/sharedComponents.style"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Container>
-     <img src={RoundLogo}  alt="logo"/>
+      <Main>
+        <div>
+          <BoldText style={{ marginBottom: 100 }}>
+            Hire the best talent in digital transformation. Grow your business.
+            Get ahead.
+          </BoldText>
+          <CtaText>Let us find your next hire</CtaText>
+        </div>
+        <BoldText>
+          We’ll connect you to professionals with the skills and experience your
+          business needs.
+        </BoldText>
+      </Main>
+      <ServicesHome />
+      <Paul />
+      <Contact />
     </Container>
   </Layout>
 )
@@ -19,7 +37,20 @@ const Container = styled.div`
   display: flex;
   flex-grow: 1;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 20px;
+  text-align: center;
+  margin: auto;
+`
+
+const Main = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 20vh 30vw;
+`
+
+const CtaText = styled.a`
+  text-decoration: underline;
+  font-size: 30px;
+  font-family: panda;
 `
